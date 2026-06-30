32d088d25b45d11687fcadfacc686ceb0dd23fef
AhiruNeko <AhiruNeko@outlook.com>
2026-06-30 19:18:57 +0800

    fix RoPE

--- Embedding.py
+++ Embedding.py
@@ -13,7 +13,7 @@ class TransformerEmbedding(nn.Module):
 
 
 class RotaryEmbedding(nn.Module):
-    def __init__(self, dim, max_len=192, base=10000):
+    def __init__(self, dim, max_len=196, base=10000):
         super().__init__()
         self.dim = dim
 
@@ -24,8 +24,8 @@ class RotaryEmbedding(nn.Module):
         freqs = torch.outer(t, self.inv_freq)
         emb = torch.cat((freqs, freqs), dim=-1)
 
-        self.register_buffer("cos_cached", emb.cos(), persistent=False)
-        self.register_buffer("sin_cached", emb.sin(), persistent=False)
+        self.register_buffer("cos_cached", emb.cos().unsqueeze(0).unsqueeze(0), persistent=False)
+        self.register_buffer("sin_cached", emb.sin().unsqueeze(0).unsqueeze(0), persistent=False)
 
     def _rotate_half(self, x):
         x1 = x[..., :self.dim // 2]
@@ -33,7 +33,8 @@ class RotaryEmbedding(nn.Module):
         return torch.cat((-x2, x1), dim=-1)
 
     def forward(self, x):
-        seq_len = x.size(1)
-        cos = self.cos_cached[:seq_len, :].unsqueeze(0)
-        sin = self.sin_cached[:seq_len, :].unsqueeze(0)
+        seq_len = x.size(2)
+        cos = self.cos_cached[:, :, :seq_len, :]
+        sin = self.sin_cached[:, :, :seq_len, :]
+
         return (x * cos) + (self._rotate_half(x) * sin)
